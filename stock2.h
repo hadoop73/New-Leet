9ae5fb4149ffa6250862aae174514afb161a242c
hadoop73 <python35@163.com>
2016-03-28 11:39:40 +0800

    风口的猪-中国牛市

---
+++ stock2.h
@@ -0,0 +1,23 @@
+//
+// Created by JQK on 2016/3/28.
+//
+
+#ifndef NEW_LEET_STOCK2_H
+#define NEW_LEET_STOCK2_H
+
+#include <vector>
+
+class Solution {
+public:
+    /**
+     * 计算你能获得的最大收益
+     *
+     * @param prices Prices[i]即第i天的股价
+     * @return 整型
+     */
+    int calculateMax(vector<int> prices) {
+        return 0;
+    }
+};
+
+#endif //NEW_LEET_STOCK2_H

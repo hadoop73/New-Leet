c70ce7bce3d6efce2ec453279665c67ddcc496c0
hadoop73 <python35@163.com>
2016-03-29 21:07:38 +0800

    LeetCode------115. Distinct Subsequences

---
+++ numDistinct.h
@@ -0,0 +1,26 @@
+//
+// Created by JQK on 2016/3/29.
+//
+
+#ifndef NEW_LEET_NUMDISTINCT_H
+#define NEW_LEET_NUMDISTINCT_H
+
+#include "header_file.h"
+
+/*
+ * 115. Distinct Subsequences
+ * https://leetcode.com/problems/distinct-subsequences/
+ * 题目：求从s中可以多少t的子字串
+ * 思路：
+ * https://leetcode.com/discuss/2143/any-better-solution-that-takes-less-than-space-while-in-time
+ *
+ */
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+
+    }
+};
+
+#endif //NEW_LEET_NUMDISTINCT_H

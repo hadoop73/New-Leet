beffc0bf8d295386c868971cb9c42e597a1e3520
hadoop73 <python35@163.com>
2016-03-29 11:41:01 +0800

    微信红包

---
+++ gift.h
@@ -0,0 +1,29 @@
+//
+// Created by JQK on 2016/3/29.
+//
+
+#ifndef NEW_LEET_GIFT_H
+#define NEW_LEET_GIFT_H
+
+#include "header_file.h"
+
+class Gift {
+public:
+    int getValue(vector<int> gifts, int n) {
+        // write code here
+        int index = 0, cnt = 1;
+        for (int i = 1; i < n; ++i) {
+            if (gifts[i]==gifts[index])
+                cnt++;
+            else cnt--;
+            if (cnt<0){
+                index =  i;
+                cnt = 1;
+            }
+        }
+        return gifts[index];
+    }
+};
+
+
+#endif //NEW_LEET_GIFT_H
---
+++ header_file.h
@@ -0,0 +1,14 @@
+//
+// Created by JQK on 2016/3/29.
+//
+
+#ifndef NEW_LEET_HEADER_FILE_H
+#define NEW_LEET_HEADER_FILE_H
+
+
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+#endif //NEW_LEET_HEADER_FILE_H

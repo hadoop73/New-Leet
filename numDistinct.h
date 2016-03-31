f6329ce4ce257660fe758f65b95b6dff9f26c1f9
hadoop73 <python35@163.com>
2016-03-31 17:30:21 +0800

    numDistinct

--- numDistinct.h
+++ numDistinct.h
@@ -16,6 +16,19 @@
  *
  */
 
+/**
+ * Solution (DP):
+ * We keep a m*n matrix and scanning through string S, while
+ * m = T.length() + 1 and n = S.length() + 1
+ * and each cell in matrix Path[i][j] means the number of distinct subsequences of
+ * T.substr(1...i) in S(1...j)
+ *
+ * Path[i][j] = Path[i][j-1]            (discard S[j])
+ *              +     Path[i-1][j-1]    (S[j] == T[i] and we are going to use S[j])
+ *                 or 0                 (S[j] != T[i] so we could not use S[j])
+ * while Path[0][j] = 1 and Path[i][0] = 0.
+ */
+
 class Solution {
 public:
     int numDistinct(string s, string t) {
@@ -28,7 +41,7 @@ public:
         }
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
-                v[j][i] = v[j][i-1] + (t[j]==s[i]?v[j-1][i-1]:0);
+                v[j][i] = v[j][i-1] + (t[j-1]==s[i-1]?v[j-1][i-1]:0);
             }
         }
         return v[n][m];
